bf1965773b5c51a3b71e2a73065f961d319c1745
shirishb-webonise <bbshirish@gmail.com>
2016-09-14 19:21:20 +0530

    adds build.gradle and test directory with sample test

---
+++ src/test/groovy/myTest.groovy
@@ -0,0 +1,11 @@
+import spock.lang.Specification
+
+/**
+ * Created by webonise on 14/9/16.
+ */
+class myTest extends Specification {
+    def "sample test"() {
+        expect:
+          true;
+    }
+}
